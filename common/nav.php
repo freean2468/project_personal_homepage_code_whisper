be7c06cb7680ac1641a1b71023b746a029892e07
dodo0211 <freean2468@gmail.com>
2021-04-21 13:05:08 +0900

    이미지 슬라이드 변경

--- common/nav.php
+++ common/nav.php
@@ -4,16 +4,88 @@
             <div class='post photoSet 1photos'>
                 <div class='slideshow'>
                     <div class='slide'>
-                        <img src='https://i.imgur.com/76nykRK.gif' />
+                        <a href="https://youtu.be/jU-9tvIG8To" target="_blank">
+                            <div>android - scalatra - mysql 로 구현한 다이어리 앱</div>
+                            <img src="https://i9.ytimg.com/vi_webp/jU-9tvIG8To/mqdefault.webp?time=1618974600000&sqp=CIiv_oMG&rs=AOn4CLAg1jk1kiKLqBPtPbYi53sXcmq5kQ" alt="android - scalatra - mysql 로 구현한 다이어리 앱"/>
+                        </a>
                     </div>
                     <div class='slide'>
-                        <img src='https://i.imgur.com/x4nn8FD.jpg' />
+                        <a href="https://youtu.be/-PFEnU79u6o" target="_blank">
+                            <div>android - python - scalatra - mysql 을 이용한 <br/>주식 종목 분석 프로그램</div>
+                            <img src="https://i9.ytimg.com/vi/-PFEnU79u6o/mq2.jpg?sqp=CIy2_oMG&rs=AOn4CLBoUOi4o1qU30FlCTMUESWd6jaqpw" alt="android - python - scalatra - mysql 을 이용한 주식 종목 분석 프로그램"/>
+                        </a>
                     </div>
                     <div class='slide'>
-                        <img src='https://i.imgur.com/rPgXc03.jpg' />
+                        <a href="https://youtu.be/P3q5QdCy21c" target="_blank">
+                            <div>Android(java) - Scalatra - PostgreSQL 을 활용한 <br/>mp3 cloud system</div>
+                            <img src="https://i9.ytimg.com/vi/P3q5QdCy21c/mq2.jpg?sqp=CIy2_oMG&rs=AOn4CLCUn43SPPdi5TVnYPfY3oO0m_OTYA" alt="Android(java) - Scalatra - PostgreSQL 을 활용한 mp3 cloud system"/>
+                        </a>
                     </div>
                     <div class='slide'>
-                        <img src='https://i.imgur.com/bUBp4t0.jpg' />
+                        <a href="https://youtu.be/4fdP1kN1_oI" target="_blank">
+                            <div>react - express.js - mongodb를 이용해 구현한 웹앱 서비스</div>
+                            <img src="https://i9.ytimg.com/vi/4fdP1kN1_oI/mqdefault.jpg?time=1618975800000&sqp=CLi4_oMG&rs=AOn4CLCoWcIwySTJeioBTpakAbRtdhIJtg" alt="react - express.js - mongodb를 이용해 구현한 웹앱 서비스" />
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/H66-IoH6034" target="_blank">
+                            <div>Cell & Data (인디게임) with C++ & DirextX9</div>
+                            <img src="https://i9.ytimg.com/vi/H66-IoH6034/mq2.jpg?sqp=CLi4_oMG&rs=AOn4CLC8V3IxXE_w7PFeCmzpaHb8kG4Qjw" alt="Cell & Data (인디게임) with C++ & DirextX9">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/bFIrVvxTiQc" target="_blank">
+                            <div>MySQL - Java - JavaFX 연동 프로젝트</div>
+                            <img src="https://i9.ytimg.com/vi/bFIrVvxTiQc/mq2.jpg?sqp=CJC9_oMG&rs=AOn4CLBZPP2l5NGzW58zy8dHgnov4XK6dg" alt="MySQL - Java - JavaFX 연동 프로젝트">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/3-cifwkFur0" target="_blank">
+                            <div>자바로 만든 MP3 플레이어 with Java, JavaFX, mp3spi</div>
+                            <img src="https://i9.ytimg.com/vi/3-cifwkFur0/mq2.jpg?sqp=CJC9_oMG&rs=AOn4CLASS2KG0M25v6k5Kmr-livYOuTjKw" alt="자바로 만든 MP3 플레이어 with Java, JavaFX, mp3spi">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/3C389bJMgCg">
+                            <div>존 덴버 - Take Me Home Country Roads 런치패드 시각화</div>
+                            <img src="https://i9.ytimg.com/vi/3C389bJMgCg/mq3.jpg?sqp=CJC9_oMG&rs=AOn4CLCgL-3RFobYAR_AV7i81MoDReBvBA" alt="존 덴버 - Take Me Home Country Roads 런치패드 시각화">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/UUxt6wQskPI">
+                            <div>이시국에 개발한 마스크 자판기 프로그램 with C</div>
+                            <img src="https://i9.ytimg.com/vi/UUxt6wQskPI/mqdefault.jpg?time=1618976700000&sqp=CLy__oMG&rs=AOn4CLB4vjpasNrLaQBNj1AtIjVufm9_Gg" alt="이시국에 개발한 마스크 자판기 프로그램 with C">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/ixeKEfR4aVk">
+                            <div>영어 타자 연습 프로그램 with C</div>
+                            <img src="https://i9.ytimg.com/vi/ixeKEfR4aVk/mq2.jpg?sqp=CLy__oMG&rs=AOn4CLBtK_7KcRNIhSwwXhjNCmgtiZF9fA" alt="영어 타자 연습 프로그램 with C">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/W6Nxgye5nfk">
+                            <div>Syn Cole - Time (소리 시각화 with Javascript & threeJS)</div>
+                            <img src="https://i9.ytimg.com/vi/W6Nxgye5nfk/mq2.jpg?sqp=CLy__oMG&rs=AOn4CLCSrVkURwOKfL0edD04q82Ezbn8GA" alt="Syn Cole - Time (소리 시각화 with Javascript & threeJS)">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/HTJD6oPVP-o">
+                            <div>Scrap irons (인디게임) with C++ & DirectX9</div>
+                            <img src="https://i9.ytimg.com/vi/HTJD6oPVP-o/mqdefault.jpg?time=1618976700000&sqp=CLy__oMG&rs=AOn4CLCKFC99QyVH2AIB92HK_8JQxYKtjg" alt="Scrap irons (인디게임) with C++ & DirectX9">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/Hps6EmFi4_w">
+                            <div>Black Sky (인디게임) with C++ & WinAPI</div>
+                            <img src="https://i9.ytimg.com/vi/Hps6EmFi4_w/mqdefault.jpg?time=1618976700000&sqp=CLy__oMG&rs=AOn4CLB1Aw2Nigm5Qdih5XsJnTacV3SMzw" alt="Black Sky (인디게임) with C++ & WinAPI">
+                        </a>
+                    </div>
+                    <div class="slide">
+                        <a href="https://youtu.be/3_vzqIr4e30">
+                            <div>puffpuff (팩맨류 게임) with C & WinAPI</div>
+                            <img src="https://i9.ytimg.com/vi/3_vzqIr4e30/mq1.jpg?sqp=COjB_oMG&rs=AOn4CLB8RSW_TPcrk869tiBn5H8dwCYIxQ" alt="puffpuff (팩맨류 게임) with C & WinAPI">
+                        </a>
                     </div>
                 </div>
             </div>
